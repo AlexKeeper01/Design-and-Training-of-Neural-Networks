d58e5dde92c96aff98bbdcfd60f47cbd10bbd90d
p1v0zavr1k <60043091+AlexKeeper01@users.noreply.github.com>
2026-03-30 01:17:39 +0300

    Update pract4.py

--- pract4.py
+++ pract4.py
@@ -33,11 +33,7 @@ weather_patterns = torch.tensor([
 
 labels = torch.tensor([[1], [1], [0], [0], [1]], dtype=torch.float32)
 
-
 model = RBFNet(in_features=4, num_centers=5)
-
-with torch.no_grad():
-    model.centers.copy_(weather_patterns)
 
 criterion = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.1)
@@ -46,7 +42,6 @@ epochs = 200
 for epoch in range(epochs):
     y_pred = model(weather_patterns)
     loss = criterion(y_pred, labels)
-
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
